fbc9e9a61fe2d9ae365a6fe88d257511256044b7
George <georgeneves.dev@gmail.com>
2023-09-10 09:57:01 -0300

    feat[#2]: Gerando todos os Repository's

---
+++ src/main/java/com/clinica/repository/ConsultaRepository.java
@@ -0,0 +1,8 @@
+package com.clinica.repository;
+
+import com.clinica.entity.ConsultaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Integer> {
+
+}
---
+++ src/main/java/com/clinica/repository/EspecialidadeRepository.java
@@ -0,0 +1,8 @@
+package com.clinica.repository;
+
+import com.clinica.entity.EspecialidadeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EspecialidadeRepository extends JpaRepository<EspecialidadeEntity, Integer> {
+
+}
