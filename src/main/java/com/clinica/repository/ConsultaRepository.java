a3ab69e088bc6e817b9a59d3acff3e269d2c470c
George <georgeneves.dev@gmail.com>
2023-09-11 19:20:49 -0300

    feat[#2]: adicionando a annotation @Repository

--- src/main/java/com/clinica/repository/ConsultaRepository.java
+++ src/main/java/com/clinica/repository/ConsultaRepository.java
@@ -2,7 +2,9 @@ package com.clinica.repository;
 
 import com.clinica.entity.ConsultaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Integer> {
 
 }
