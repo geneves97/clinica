87f676bd187d872b5423d231d4d44e6ade6324cf
George <georgeneves.dev@gmail.com>
2023-09-11 19:21:38 -0300

    feat[#2]: criando o restConsulta.findById

--- src/main/java/com/clinica/controller/ConsultaController.java
+++ src/main/java/com/clinica/controller/ConsultaController.java
@@ -1,20 +1,23 @@
 package com.clinica.controller;
 
 import com.clinica.entity.ConsultaEntity;
+import com.clinica.rest.RestConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ConsultaController {
 
     @Autowired
-    private ConsultaBusiness consultaBusiness;
+    private RestConsulta restConsulta;
 
+    @GetMapping("consulta/{id}")
+    public ConsultaEntity get(@PathVariable (value = "id") Integer id){
+        return restConsulta.findById(id);
+    }
     @PostMapping
     public ConsultaEntity criarConsulta(@RequestBody ConsultaEntity consultaEntity) throws Exception {
-    return consultaBusiness.save(consultaEntity);
+    return restConsulta.save(consultaEntity);
     }
 
 }
