2dbb6687d8123bb0a88de8a97b7d3eba8a30015b
George <georgeneves.dev@gmail.com>
2023-09-11 23:09:13 -0300

    feat[#2]: Ajustando o Autowired e adicionando .saveConsulta e .findAllConsultas

--- src/main/java/com/clinica/controller/ConsultaController.java
+++ src/main/java/com/clinica/controller/ConsultaController.java
@@ -1,23 +1,30 @@
 package com.clinica.controller;
 
 import com.clinica.entity.ConsultaEntity;
-import com.clinica.rest.RestConsulta;
+import com.clinica.service.ConsultaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ConsultaController {
 
     @Autowired
-    private RestConsulta restConsulta;
+    ConsultaService consultaService;
 
-    @GetMapping("consulta/{id}")
-    public ConsultaEntity get(@PathVariable (value = "id") Integer id){
-        return restConsulta.findById(id);
-    }
-    @PostMapping
+    @PostMapping("/consulta")
     public ConsultaEntity criarConsulta(@RequestBody ConsultaEntity consultaEntity) throws Exception {
-    return restConsulta.save(consultaEntity);
+        return consultaService.saveConsulta(consultaEntity);
     }
 
+    @GetMapping("/consulta/{id}")
+    public ConsultaEntity get(@PathVariable (value = "id") Integer id){
+        return consultaService.findConsultaById(id);
+    }
+
+    @GetMapping("/consulta")
+    public List<ConsultaEntity> get() {
+        return consultaService.findAllConsultas();
+    }
 }
