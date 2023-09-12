e19e8ff5cab4636cbb5d2c37f454b0a54b40e5d7
George <georgeneves.dev@gmail.com>
2023-09-11 23:10:48 -0300

    feat[#2]: removendo a annotation @Repository visto que ela não é necessaria no momento.

--- src/main/java/com/clinica/repository/ConsultaRepository.java
+++ src/main/java/com/clinica/repository/ConsultaRepository.java
@@ -4,7 +4,6 @@ import com.clinica.entity.ConsultaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ConsultaRepository extends JpaRepository<ConsultaEntity, Integer> {
 
 }
